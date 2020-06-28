ca307db30141555c36ea5f4a144a80286304001e
Shagnikpaul <shagnikpaul.772@gmail.com>
2020-06-29 00:34:08 +0530

    Graphs for different data (not yet finished). VERSION TWEW

---
+++ app/src/main/java/com/ace/pandemic/deathGraph.java
@@ -0,0 +1,14 @@
+package com.ace.pandemic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class deathGraph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_death_graph);
+    }
+}
